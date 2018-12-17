34982167682e4ce2862c8825c46a7df1ce981cc2
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-12-17 20:34:24 +0000

    Integrating with third party API to fetch list of movies for a search criteria

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+
 import {
   Form,
   FormControl,
@@ -6,6 +7,8 @@ import {
   ControlLabel,
   Button
 } from "react-bootstrap";
+
+import { API_KEY } from "./../secrets";
 
 class Search extends Component {
   constructor(props) {
@@ -15,9 +18,21 @@ class Search extends Component {
       query: ""
     };
   }
+
   search() {
     console.log("Search button clicked", this.state.query);
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${
+      this.state.query
+    }`;
+
+    console.log(url);
+    fetch(url, {
+      method: "GET"
+    })
+      .then(response => response.json())
+      .then(result => console.log(result));
   }
+
   render() {
     return (
       <div>
---
+++ src/secrets.js
@@ -0,0 +1,1 @@
+export const API_KEY = "c8d8c40713c2164f10bc006b246301eb";
