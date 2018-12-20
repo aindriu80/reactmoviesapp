c2f41913cd7fc1d30eb3e21351b94083c58c5d51
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-12-20 22:14:51 +0000

    Adding some fontawesome

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,12 @@ import React, { Component } from "react";
 import MovieResults from "./movieResults";
 import Search from "./Search";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faIgloo } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faIgloo);
+
 class App extends Component {
   render() {
     return (
--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -1,7 +1,23 @@
 import React, { Component } from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+
 const urlComponent = "https://image.tmdb.org/t/p/w600_and_h900_bestv2/";
 
 class MovieItem extends Component {
+  displayFav() {
+    if (!this.state.favorited) {
+      return (
+        <span
+          className=""
+          onClick={() => this.setState({ favorited: !this.state.favorited })}
+        />
+      );
+    } else {
+      return <span className="glyphicon glypicon-heart" />;
+    }
+  }
   render() {
     return (
       <div className="col-sm-12 col-sm-3">
@@ -10,15 +26,17 @@ class MovieItem extends Component {
           <div className="caption">
             <h3>{this.props.movie.title}</h3>
             <p>{this.props.movie.overview}</p>
-            <p>Release Date{this.props.movie.release_date}</p>
-            <p>Ratings{this.props.movie.vote_average}</p>
+            <p>Release Date: {this.props.movie.release_date}</p>
             <p>
-              <a href="#" className="btn btn-primary" role="button">
-                Button
-              </a>{" "}
-              <a href="#" className="btn btn-default" role="button">
-                Button
-              </a>
+              Ratings: -{" "}
+              <span>
+                <span className="badge badge-default">
+                  {this.props.movie.vote_average}
+                </span>
+              </span>
+            </p>
+            <p>
+              <i className="fa fa-heart" />
             </p>
           </div>
         </div>
