14119545df958a7e4fc6a5b6d56155d04faa0ff5
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-12-23 18:47:45 +0000

    Displaying favorite movie from the list

--- src/components/FavoriteMovieList.js
+++ src/components/FavoriteMovieList.js
@@ -1,15 +1,27 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import MovieItem from "./MovieItem";
 
 class FavoriteMovieList extends Component {
   render() {
-    return <h4>My favorite Movies</h4>;
+    // console.log(this.props.favorites);
+
+    return (
+      <div>
+        <h4>My favorite Movies</h4>
+        {this.props.favorites.map(movie => {
+          return <MovieItem movie={movie} key={movie.id} />;
+        })}
+      </div>
+    );
   }
 }
 
 function mapStateToProps(state) {
-  console.log("FavoriteMoviesList -", state);
-  return {};
+  // console.log("FavoriteMoviesList -", state);
+  return {
+    favorites: state.favorites
+  };
 }
 export default connect(
   mapStateToProps,
